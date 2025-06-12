38408d8043af699e23db0b0cdcbf49db4f58f890
ManchegoMike <mantwood@outlook.com>
2025-06-12 19:09:38 -0400

    Removed Down direction, added UpRight & UpLeft
    Moving the camera down often ends up dragging it along the ground, putting it in a "sticky" mode where you have to overcompensate to get it unstuck. It seems too disorienting to me, as it causes the player to spend an extra second correcting their view.

--- Functions/Utils/RotateScreenEffect.lua
+++ Functions/Utils/RotateScreenEffect.lua
@@ -11,27 +11,42 @@ function RotateScreenEffect()
           shake(n - 1, speed, seconds)
         end)
       end)
+    else
+      -- Failsafe in case one of the MoveView functions leaves the camera spinning.
+      C_Timer.After(0.5, function()
+        MoveViewLeftStop()
+        MoveViewRightStop()
+      end)
     end
   end
 
   local stopMovement = true
   local moveSeconds = 0.2 + 0.1*math.random()       -- was 0.2, now 0.2 - 0.3
   local moveSpeed = 2.0 + 1.5*math.random()         -- was 2.0, now 2.0 - 3.5
-  local randomNum = math.random(1, 6)               -- Pick a random direction (1-4) or the shake effect (5-6)
+  local randomNum = math.random(1, 8)               -- Pick a random direction (1-5) or the shake effect (6-8)
   if randomNum == 1 then
-    MoveViewDownStart(moveSpeed)
-  elseif randomNum == 2 then
+    --print("CRIT R")
     MoveViewRightStart(moveSpeed)
-  elseif randomNum == 3 then
+  elseif randomNum == 2 then
+    --print("CRIT L")
     MoveViewLeftStart(moveSpeed)
-  elseif randomNum == 4 then
+  elseif randomNum == 3 then
+    --print("CRIT U")
     MoveViewUpStart(moveSpeed)
-  elseif randomNum == 5 or randomNum == 6 then
+  elseif randomNum == 4 then
+    --print("CRIT UR")
+    MoveViewUpStart(moveSpeed)
+    MoveViewRightStart(moveSpeed)
+  elseif randomNum == 5 then
+    --print("CRIT UL")
+    MoveViewUpStart(moveSpeed)
+    MoveViewLeftStart(moveSpeed)
+  else
     stopMovement = false
     local moveSeconds = 0.03 + 0.02*math.random()
     local moveSpeed = 3.0 + 2.0*math.random()
     local nShakes = random(3, 5)
-    --print("CRIT: seconds=" .. moveSeconds .. ", speed=" .. moveSpeed .. ", nShakes=" .. nShakes)
+    --print("CRIT SHAKE: seconds=" .. moveSeconds .. ", speed=" .. moveSpeed .. ", nShakes=" .. nShakes)
     shake(nShakes, moveSpeed, moveSeconds)
   end
 
