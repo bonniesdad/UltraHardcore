6b28f5533badc3cd0dd37f7b242da8cedd17a20d
Bonnie's Dad <bonniesnerdydad@gmail.com>
2025-10-08 15:09:56 +0100

    Join party auto message

--- Functions/JoinParty.lua
+++ Functions/JoinParty.lua
@@ -1,47 +1,101 @@
 local frame = CreateFrame('Frame')
 
 frame:RegisterEvent('GROUP_JOINED')
+frame:RegisterEvent('GROUP_ROSTER_UPDATE')
+
+-- Track previous party members to detect new joins
+local previousPartyMembers = {}
+
+-- Function to get current party member names
+local function getCurrentPartyMembers()
+  local members = {}
+  if IsInRaid() then
+    for i = 1, GetNumGroupMembers() do
+      local name = GetRaidRosterInfo(i)
+      if name then
+        table.insert(members, name)
+      end
+    end
+  elseif IsInGroup() then
+    for i = 1, GetNumGroupMembers() do
+      local name = UnitName('party' .. i)
+      if name then
+        table.insert(members, name)
+      end
+    end
+  end
+  return members
+end
+
+-- Function to automatically post warning message
+local function postWarningMessage()
+  if IsInGroup() then
+    -- Check if there are other members besides ourselves
+    local numMembers = GetNumGroupMembers()
+    local playerName = UnitName('player')
+    local hasOtherMembers = false
+    
+    if IsInRaid() then
+      for i = 1, numMembers do
+        local name = GetRaidRosterInfo(i)
+        if name and name ~= playerName then
+          hasOtherMembers = true
+          break
+        end
+      end
+    else
+      -- For party groups, check party members
+      for i = 1, numMembers do
+        local name = UnitName('party' .. i)
+        if name and name ~= playerName then
+          hasOtherMembers = true
+          break
+        end
+      end
+    end
+    
+    -- Only post message if there are other members in the group
+    if hasOtherMembers then
+      local chatType = IsInRaid() and 'RAID' or 'PARTY'
+      SendChatMessage(
+        '[ULTRA] I am using the Ultra Hardcore addon. You are at a higher risk of death if you group with me.',
+        chatType
+      )
+    end
+  end
+end
 
 frame:SetScript('OnEvent', function(self, event, ...)
   if event == 'GROUP_JOINED' then
-    StaticPopupDialogs['ULTRA_HARDCORE_WARNING'] = {
-      text = 'You should only do dungeons and raids with other players who are using the Ultra Hardcore addon. To join a group on non ultimates is putting them in increased danger and can be seen as griefing. Would you like to disable the addon?',
-      button1 = 'Disable Addon',
-      button2 = 'No, Keep Addon On',
-      OnAccept = function()
-        local chatType = IsInRaid() and 'RAID' or 'PARTY'
-        SendChatMessage(
-          '!! INFO !! I have disabled the Ultra Hardcore addon for this group.',
-          chatType
-        )
-        SaveDBData('GLOBAL_SETTINGS', {
-          hidePlayerFrame = false,
-          hideMinimap = false,
-          hideBuffFrame = false,
-          hideTargetFrame = false,
-          hideTargetTooltip = false,
-          hideQuestFrame = false,
-          showTunnelVision = false,
-          showDazedEffect = false,
-          showCritScreenMoveEffect = false,
-          showIncomingDamageEffect = false,
-          petsDiePermanently = false,
-          hideGroupHealth = false,
-          hideActionBars = false,
-        })
-        ReloadUI()
-      end,
-      OnCancel = function()
-        local chatType = IsInRaid() and 'RAID' or 'PARTY'
-        SendChatMessage(
-          '!! BEWARE !! I am using the Ultra Hardcore addon. The chances of my demise are much higher. You have been warned.',
-          chatType
-        )
-      end,
-      timeout = 0,
-      whileDead = true,
-      hideOnEscape = true,
-    }
-    StaticPopup_Show('ULTRA_HARDCORE_WARNING')
+    -- Automatically post warning message when joining a group
+    postWarningMessage()
+    
+    -- Update party member tracking
+    previousPartyMembers = getCurrentPartyMembers()
+    
+  elseif event == 'GROUP_ROSTER_UPDATE' then
+    -- Check if someone new joined the party
+    local currentMembers = getCurrentPartyMembers()
+    local playerName = UnitName('player')
+    
+    -- Check if we have new members (excluding ourselves)
+    for _, member in ipairs(currentMembers) do
+      local isNewMember = true
+      for _, prevMember in ipairs(previousPartyMembers) do
+        if member == prevMember then
+          isNewMember = false
+          break
+        end
+      end
+      
+      -- If it's a new member and not ourselves, post warning message
+      if isNewMember and member ~= playerName then
+        postWarningMessage()
+        break -- Only post once per roster update
+      end
+    end
+    
+    -- Update party member tracking
+    previousPartyMembers = currentMembers
   end
 end)
