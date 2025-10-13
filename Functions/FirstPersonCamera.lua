4e638bac4fee60e5ddfe27c81897efac8fdb2fbb
Bonnie's Dad <bonniesnerdydad@gmail.com>
2025-10-13 19:18:52 +0100

    Cleanup first person not active

--- Functions/FirstPersonCamera.lua
+++ Functions/FirstPersonCamera.lua
@@ -6,11 +6,6 @@ local function zoomInHard()
   CameraZoomIn(50)
 end
 
-local function zoomOutHard()
-  CameraZoomOut(50)
-end
-
-
 local function startTicker()
   if ticker then return end
   ticker = C_Timer.NewTicker(TICK_INTERVAL, function()
@@ -21,31 +16,15 @@ local function startTicker()
   end)
 end
 
-local function stopTicker()
-  if ticker then
-    ticker:Cancel()
-    ticker = nil
-  end
-end
-
-
 local function enableLock()
   enabled = true
   zoomInHard()
   startTicker()
 end
 
-local function disableLock()
-  enabled = false
-  stopTicker()
-  zoomOutHard()
-end
-
 -- PUBLIC API (unchanged)
 function ForceFirstPersonCamera(forceFirstPerson)
   if forceFirstPerson then
     enableLock()
-  else
-    disableLock()
   end
 end
