60c6eaf7c4effc3d677aa28608aeccc55ecad7d4
BonniesDad <bonniesNerdyDad@gmail.com>
2025-10-17 09:21:33 -0500

    Refactoring party death audio tests into a dedicated file in Funtion Utils

---
+++ Functions/Utils/PartyDeathAudio.lua
@@ -0,0 +1,21 @@
+local currentSoundHandle = nil
+
+SLASH_STOPSOUND1 = '/stopsound'
+SlashCmdList.STOPSOUND = function()
+  if currentSoundHandle then
+    StopSound(currentSoundHandle)
+    currentSoundHandle = nil
+  end
+end
+
+SLASH_TESTPARTYDEATHAUDIO1 = '/testpartydeathaudio'
+SlashCmdList.TESTPARTYDEATHAUDIO = function()
+  local randomNumber = random(1,4)
+  local willPlay = nil
+  local soundHandle = nil
+  -- Play sound file on party death
+  willPlay, soundHandle = PlaySoundFile("Interface\\AddOns\\UltraHardcore\\Sounds\\PartyDeath" .. randomNumber .. ".ogg", "SFX")
+  if willPlay then
+    currentSoundHandle = soundHandle
+  end
+end
