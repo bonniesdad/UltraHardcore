b004d66e002c5e383d23cd4061c547422b0a7b66
Bonnie's Dad <bonniesnerdydad@gmail.com>
2025-10-28 22:53:01 +0000

    Resource bar colour selector

--- Functions/Utils/Resource/GetPowerTypeColor.lua
+++ Functions/Utils/Resource/GetPowerTypeColor.lua
@@ -1,10 +1,17 @@
-
 -- Get the color for a power type
 function GetPowerTypeColor(powerType)
-    if not powerType or type(powerType) ~= "string" then
-        print("UltraHardcore: Invalid power type provided to GetPowerTypeColor")
-        return unpack(POWER_COLORS.MANA)
-    end
+  if not powerType or type(powerType) ~= 'string' then
+    print('UltraHardcore: Invalid power type provided to GetPowerTypeColor')
+    return unpack(POWER_COLORS.MANA)
+  end
 
-    return unpack(POWER_COLORS[powerType] or POWER_COLORS.MANA)
+  -- Prefer per-character override if present in settings
+  if GLOBAL_SETTINGS and GLOBAL_SETTINGS.resourceBarColors and GLOBAL_SETTINGS.resourceBarColors[powerType] then
+    local c = GLOBAL_SETTINGS.resourceBarColors[powerType]
+    if type(c) == 'table' and #c >= 3 then
+      return c[1], c[2], c[3]
+    end
+  end
+
+  return unpack(POWER_COLORS[powerType] or POWER_COLORS.MANA)
 end
