794ff9d33871d4160c75f352207ad4435f475d91
BonniesDad <bonniesNerdyDad@gmail.com>
2025-10-28 15:10:43 -0400

    Added an indicator color to unit name when tapped by another player

--- Functions/SetTargetTooltipDisplay.lua
+++ Functions/SetTargetTooltipDisplay.lua
@@ -9,6 +9,14 @@ function SetTargetTooltipDisplay(hideTargetTooltip)
       -- Hide health bar graphic under tooltip
       GameTooltipStatusBar:Hide()
 
+      -- Check if unit is tapped by another player and in combat, modify first line (name) color
+      if not UnitIsTappedByPlayer(unit) and UnitAffectingCombat(unit) then
+        local nameLine = _G['GameTooltipTextLeft1']
+        if nameLine then
+          nameLine:SetTextColor(0.5, 0.5, 0.5) -- Gray color
+        end
+      end
+
       -- Modify tooltip lines
       for i = 2, self:NumLines() do
         local line = _G['GameTooltipTextLeft' .. i]
