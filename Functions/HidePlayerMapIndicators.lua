2eb0fdb54c5d968f6dd7f722f43164d7ace4fcda
BonniesDad <bonniesNerdyDad@gmail.com>
2025-10-28 21:52:12 +0000

    Route planner hide minimap marker

--- Functions/HidePlayerMapIndicators.lua
+++ Functions/HidePlayerMapIndicators.lua
@@ -1,18 +1,23 @@
-local HIDE_ALL = true -- set to false to hide only the player
+function HidePlayerMapIndicators()
+  if not GLOBAL_SETTINGS.routePlanner then return end
 
--- Minimap: hide player arrow
-if Minimap and Minimap.SetPlayerTexture then
-    Minimap:SetPlayerTexture("")
-end
+  local HIDE_ALL = true -- set to false to hide only the player
+  -- Minimap: hide player arrow
+  if Minimap and Minimap.SetPlayerTexture then
+    Minimap:SetPlayerTexture('')
+  end
 
--- World Map: kill the yellow ping + hide group/player pins
-for pin in WorldMapFrame:EnumeratePinsByTemplate("GroupMembersPinTemplate") do
-    hooksecurefunc(pin, "StartPlayerPing", function(self) self:StopPlayerPing() end)
+  -- World Map: kill the yellow ping + hide group/player pins
+  for pin in WorldMapFrame:EnumeratePinsByTemplate('GroupMembersPinTemplate') do
+    hooksecurefunc(pin, 'StartPlayerPing', function(self)
+      self:StopPlayerPing()
+    end)
     if HIDE_ALL then
-        pin:Hide()
-        pin.Show = function() end
+      pin:Hide()
+      pin.Show = function() end
     else
-        pin:SetPinTexture("player", "")
-        break
+      pin:SetPinTexture('player', '')
+      break
     end
+  end
 end
