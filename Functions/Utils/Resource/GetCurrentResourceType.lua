0ccc6b0de419f97c99a073e4684eb87bd81ea7ab
BonniesDad <bonniesNerdyDad@gmail.com>
2025-10-30 20:59:24 +0000

    Druid resource bar update

--- Functions/Utils/Resource/GetCurrentResourceType.lua
+++ Functions/Utils/Resource/GetCurrentResourceType.lua
@@ -1,16 +1,19 @@
 function GetCurrentResourceType()
-    local _, playerClass = UnitClass('player')
-    local form = GetShapeshiftFormID()
-    -- 1 for Cat Form
-    -- 5 for Bear Form
-    -- 31 for Dire Bear Form
-    -- 3 for Travel Form
-    -- 4 for Aquatic Form
-    if playerClass == 'ROGUE' or (playerClass == 'DRUID' and form == 1) then
-      return 'ENERGY'
-    elseif playerClass == 'WARRIOR' or (playerClass == 'DRUID' and (form == 5 or form == 31)) then
-      return 'RAGE'
-    end
-    return 'MANA'
+  local _, playerClass = UnitClass('player')
+  local form = GetShapeshiftFormID()
+  -- Aquatic Form - 4
+  -- Bear Form - 5 (BEAR_FORM constant)
+  -- Cat Form - 1 (CAT_FORM constant)
+  -- Flight Form - 29
+  -- Moonkin Form - 31 - 35 (MOONKIN_FORM constant) (different races seem to have different numbers)
+  -- Swift Flight Form - 27
+  -- Travel Form - 3
+  -- Tree of Life - 2
+  -- Treant Form - 36
+  if playerClass == 'ROGUE' or (playerClass == 'DRUID' and form == 1) then
+    return 'ENERGY'
+  elseif playerClass == 'WARRIOR' or (playerClass == 'DRUID' and (form == 5)) then
+    return 'RAGE'
   end
-  
+  return 'MANA'
+end
