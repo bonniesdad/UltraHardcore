1086f62f91011c408b790da706fdfcc326cbab75
Bonnie's Dad <bonniesnerdydad@gmail.com>
2025-10-30 21:49:06 +0000

    Dire bear form rage bar

--- Functions/Utils/Resource/GetCurrentResourceType.lua
+++ Functions/Utils/Resource/GetCurrentResourceType.lua
@@ -1,6 +1,12 @@
 function GetCurrentResourceType()
   local _, playerClass = UnitClass('player')
   local form = GetShapeshiftFormID()
+  --   1 - Cat Form
+  -- 3 - Travel Form
+  -- 4 - Aquatic Form
+  -- 5 - Bear Form
+  -- 8 - Dire Bear Form
+
   -- Aquatic Form - 4
   -- Bear Form - 5 (BEAR_FORM constant)
   -- Cat Form - 1 (CAT_FORM constant)
@@ -12,7 +18,7 @@ function GetCurrentResourceType()
   -- Treant Form - 36
   if playerClass == 'ROGUE' or (playerClass == 'DRUID' and form == 1) then
     return 'ENERGY'
-  elseif playerClass == 'WARRIOR' or (playerClass == 'DRUID' and (form == 5)) then
+  elseif playerClass == 'WARRIOR' or (playerClass == 'DRUID' and (form == 5 or form == 8)) then
     return 'RAGE'
   end
   return 'MANA'
