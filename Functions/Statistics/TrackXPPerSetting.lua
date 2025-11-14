0aaa13b7e00ca8b1ee03710a6af4496d9d3ddd14
Wootenblatz <wootenblatz@gmail.com>
2025-11-14 12:35:36 -0600

    Refactoring Addon XP Tracking into its own file

--- Functions/Statistics/TrackXPPerSetting.lua
+++ Functions/Statistics/TrackXPPerSetting.lua
@@ -41,27 +41,6 @@ local settingToXPVariable = {
 local sessionStartXP = nil
 local lastXPUpdate = nil
 local lastXPValue = nil
-local xpTrackingInitialized = false
-
-local function InitializeXpGainedWithAddon(playerLevel, lastXPValue, stats)
-  if lastXPValue == 0 and playerLevel > 1 then
-    -- This shouldn't happen but just in case, don't run until later
-    return false
-  end
-
-  CharacterStats:UpdateStat("xpTotal", CharacterStats:CalculateTotalXPGained())
-
-  local shouldRecalculate = CharacterStats:ShouldRecalculateXPGainedWithAddon(stats)
-
-  if playerLevel == 1 and lastXPValue == 0 then
-    CharacterStats:UpdateStat("xpTotal", 0)
-    CharacterStats:UpdateStat("xpGWA", 0)
-    CharacterStats:UpdateStat("xpGWOA", 0)
-  elseif shouldRecalculate  == true then
-    CharacterStats:ResetXPGainedWithAddon(playerLevel, true)
-  end
-  return true
-end
 
 -- Function to initialize XP tracking
 local function InitializeXPTracking()
@@ -69,31 +48,15 @@ local function InitializeXPTracking()
   -- Start tracking from current XP for this session
   lastXPValue = UnitXP("player")
   lastXPUpdate = GetTime()
-
-  -- This code only needs to run once either on login or reload
-  if xpTrackingInitialized ~= true then
-    local stats = CharacterStats:GetCurrentCharacterStats()
-    InitializeXpGainedWithAddon(playerLevel, lastXPValue, stats)
-    xpTrackingInitialized = true
-  end
+  AddonXPTracking:Initialize(lastXPValue)
 end
 
 -- Function to update XP tracking for each setting
 local function UpdateXPTracking(levelUp)
   if levelUp == nil then levelUp = false end
 
-  local currentXP = 0
+  local currentXP = AddonXPTracking:GetXP(levelUp)
   local currentTime = GetTime()
-
-  if levelUp then
-    -- The game does not report the correct XP amount on level up, so we calculate it
-    local newLevel = UnitLevel("player")
-    local xpThisLevel = TotalXPTable[newLevel]
-    currentXP = xpThisLevel
-  else
-    currentXP = UnitXP("player")
-  end
-
   
   -- Only update if XP has increased and enough time has passed (prevent spam)
   if currentXP > lastXPValue and (currentTime - lastXPUpdate) > 1 then
@@ -105,27 +68,15 @@ local function UpdateXPTracking(levelUp)
       local isSettingEnabled = GLOBAL_SETTINGS[settingName]
       
       -- For boolean settings, if they're false, we're gaining XP "without" that option
-      if not isSettingEnabled or xpVariable == "xpGWA" or xpVariable == "xpGWOA" or xpVariable == "xpTotal" then
-        -- We do not want to track xpGWOA while the addon is running even though the setting is always false
-        if xpVariable ~= "xpGWOA" then 
-          local currentXPForSetting = CharacterStats:GetStat(xpVariable) or 0
-          local newXPForSetting = currentXPForSetting + xpGained
-          CharacterStats:UpdateStat(xpVariable, newXPForSetting)
-        end
+      if not isSettingEnabled or AddonXPTracking:ShouldTrackStat(xpVariable) then
+        local currentXPForSetting = CharacterStats:GetStat(xpVariable) or 0
+        local newXPForSetting = currentXPForSetting + xpGained
+        CharacterStats:UpdateStat(xpVariable, newXPForSetting)
       end
     end
     
-    if levelUp then
-      -- We just leveled up so our UnitXP is going to be only the amount in excess of the level up threshhold
-      -- Leveling up means our UnitXP is effectively 0, so set that value
-      --
-      -- Do not reset the time because UpdateXPTracking is going to fire again immediately
-      -- after PLAYER_LEVEL_UP and we want to count that XP
-      lastXPValue = 0
-    else
-      lastXPValue = currentXP
-      lastXPUpdate = currentTime
-    end
+    lastXPValue = AddonXPTracking:NewLastXPValue(levelUp, currentXP)
+    lastXPUpdate = currentTime
   end
 end
 
@@ -155,11 +106,7 @@ xpTrackingFrame:SetScript("OnEvent", function(self, event, ...)
   if event == "PLAYER_XP_UPDATE" then
     UpdateXPTracking(false)
   elseif event == "PLAYER_LEVEL_UP" then
-    -- Reset tracking when leveling up
     UpdateXPTracking(true)
-    lastXPValue = 0
-    lastXPUpdate = lastXPUpdate - 1
-    --InitializeXPTracking()
   elseif event == "PLAYER_LOGIN" then
     InitializeXPTracking()
   elseif event == "ADDON_LOADED" and select(1, ...) == "UltraHardcore" then
