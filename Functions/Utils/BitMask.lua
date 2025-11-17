3e3c2424fe186d758fc254572be1dbd468fe39e1
Booji <55899473+BoojiB@users.noreply.github.com>
2025-11-16 23:56:21 -0500

    Move bitmask functions to their own file

---
+++ Functions/Utils/BitMask.lua
@@ -0,0 +1,15 @@
+-- Maybe move these to a globals file?
+MINIMAP_FLAG_CLOCK  = 0x01 -- 0001
+MINIMAP_FLAG_MAIL = 0x02 -- 0010
+
+function HasFlag(mask, flag)
+  return bit.band(mask, flag) ~= 0
+end
+
+function AddFlag(mask, flag)
+  return bit.bor(mask, flag)
+end
+
+function RemoveFlag(mask, flag)
+  return bit.band(mask, bit.bnot(flag))
+end
