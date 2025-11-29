7520810fcca3b4c75031ab69e99cddd2f5320275
Booji <55899473+BoojiB@users.noreply.github.com>
2025-11-29 13:50:09 -0500

    Remove hardcoded on-screen sats now that DB load is proper

--- Settings/Settings.lua
+++ Settings/Settings.lua
@@ -16,11 +16,7 @@ local function shouldRadioBeChecked(settingName, settings)
   if settings[settingName] ~= nil then
     return settings[settingName]
   else
-    if settingName == 'showMainStatisticsPanelLevel' or settingName == 'showMainStatisticsPanelLowestHealth' or settingName == 'showMainStatisticsPanelEnemiesSlain' or settingName == 'showMainStatisticsPanelDungeonsCompleted' or settingName == 'showMainStatisticsPanelHighestCritValue' or settingName == 'showMainStatisticsPanelCloseEscapes' then
-      return true
-    else
-      return false
-    end
+    return false
   end
 end
 
