3cb37b8258f698366620083b7e46d97dc9619673
bonniesdad <bonniesnerdydad@gmail.com>
2025-12-08 16:26:44 +0000

    Dependancy on hideTargetFrame

--- Settings/SettingsOptionsTab.lua
+++ Settings/SettingsOptionsTab.lua
@@ -86,18 +86,22 @@ local settingsCheckboxOptions = { {
   name = 'Completely Remove Target Frame',
   dbSettingsValueName = 'completelyRemoveTargetFrame',
   tooltip = 'Completely remove the target frame',
+  dependsOn = 'hideTargetFrame',
 }, {
   name = 'Show Target Buffs',
   dbSettingsValueName = 'showTargetBuffs',
   tooltip = 'Show buffs on the target frame',
+  dependsOn = 'hideTargetFrame',
 }, {
   name = 'Show Target Debuffs',
   dbSettingsValueName = 'showTargetDebuffs',
   tooltip = 'Show debuffs on the target frame',
+  dependsOn = 'hideTargetFrame',
 }, {
   name = 'Show Target Raid Icon',
   dbSettingsValueName = 'showTargetRaidIcon',
   tooltip = 'Show raid icon on the target frame',
+  dependsOn = 'hideTargetFrame',
 }, {
   -- Misc Settings (no preset button)
   name = 'On Screen Statistics',
