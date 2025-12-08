40446b911207c3cef365b8ce26df3ff8d7f2c061
Booji <55899473+BoojiB@users.noreply.github.com>
2025-12-08 16:07:10 -0500

    Put back delay for loss of control (taxi)

--- Functions/SetActionBarVisibility.lua
+++ Functions/SetActionBarVisibility.lua
@@ -95,6 +95,11 @@ f:SetScript('OnEvent', function(self, event, ...)
   elseif event == 'PLAYER_ENTERING_WORLD' then
     SetActionBarVisibility(GLOBAL_SETTINGS.hideActionBars)
   elseif event == 'PLAYER_CONTROL_GAINED' or event == 'PLAYER_CONTROL_LOST' then
+    -- We need a slight delay after getting on a taxi before UnitOnTaxi will return true
+    if C_Timer and C_Timer.After then
+      C_Timer.After(0.2, function()
     SetActionBarVisibility(GLOBAL_SETTINGS.hideActionBars)
+      end)
+    end
   end
 end)
