d9adc2a4c41973380d4fc93b9e448dbbc379d39a
BonniesDad <bonniesnerdydad@gmail.com>
2025-12-14 19:13:15 +0000

    Player comms?

--- Constants/PresetSections.lua
+++ Constants/PresetSections.lua
@@ -30,6 +30,7 @@ local PRESET_SECTIONS = { {
     'showFullHealthIndicator',
     'hideCustomResourceBar',
     'showHealingIndicator',
+    'showWildAllyHealthIndicator',
     'setFirstPersonCamera',
     'completelyRemovePlayerFrame',
     'completelyRemoveTargetFrame',
