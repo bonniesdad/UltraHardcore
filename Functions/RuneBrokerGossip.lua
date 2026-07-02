abe081a3f86f123961196d900c4e9261edf1b297
bonniesdad <bonniesnerdydad@gmail.com>
2026-07-02 11:10:47 +0100

    allow rune vendor

--- Functions/RuneBrokerGossip.lua
+++ Functions/RuneBrokerGossip.lua
@@ -1,16 +1,16 @@
-local RUNE_BROKER_NAMES = { ['Rune Broker'] = true }
+-- local RUNE_BROKER_NAMES = { ['Rune Broker'] = true }
 
-local frame = CreateFrame('Frame')
-frame:RegisterEvent('GOSSIP_SHOW')
-frame:SetScript('OnEvent', function()
-  local name = UnitName('npc')
-  if name and RUNE_BROKER_NAMES[name] then
-    HideUIPanel(GossipFrame)
-    DEFAULT_CHAT_FRAME:AddMessage(
-      '|cFFFF0000[ULTRA]|r You cannot purchase runes from the Rune Broker.',
-      1,
-      0,
-      0
-    )
-  end
-end)
+-- local frame = CreateFrame('Frame')
+-- frame:RegisterEvent('GOSSIP_SHOW')
+-- frame:SetScript('OnEvent', function()
+--   local name = UnitName('npc')
+--   if name and RUNE_BROKER_NAMES[name] then
+--     HideUIPanel(GossipFrame)
+--     DEFAULT_CHAT_FRAME:AddMessage(
+--       '|cFFFF0000[ULTRA]|r You cannot purchase runes from the Rune Broker.',
+--       1,
+--       0,
+--       0
+--     )
+--   end
+-- end)
